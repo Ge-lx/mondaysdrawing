310178e494258dd7e3acbbdb970cf05f6d07ee09
ge-lx <f4lkebert@gmail.com>
2020-04-18 02:55:09 +0200

    :art: Refactor

--- frontend/states.js
+++ frontend/states.js
@@ -35,34 +35,4 @@
 
 	define('$states', () => ['state_home', 'state_draw']);
 
-	resolve((paper_canvas, PathExportTools) => {
-		var size = 0;
-
-		paper_canvas.onPathStarted((path) => {
-			// console.log('onPathStarted');
-			size += JSON.stringify(path).length * 2;
-			// setTimeout(() => {
-			// 	paper_canvas.startPath(path)
-			// }, 2000);
-		});
-		paper_canvas.onPathCompleted(() => {
-			// console.log('onPathCompleted');
-			size += 'onPathCompleted'.length * 2;
-			console.log('This path was ' + size + ' bytes');
-			size = 0;
-			// setTimeout(paper_canvas.completePath, 2000);
-		});
-
-		paper_canvas.onPathSegments((buffer) => {
-			// console.log('Segments in byte: ', 2 * buffer.length);
-			size += 3 * buffer.length;
-			// console.log('onPathSegments: ', buffer);
-			// setTimeout(() => {
-
-			// 	const segments = PathExportTools.segmentsFromArrayBuffer(buffer);
-			// 	paper_canvas.addPathSegments(segments);
-			// }, 2000);
-		});
-	});
-
 }(bnc_bunch));
