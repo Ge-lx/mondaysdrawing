358d9058e50e3e7714454dba58aef7be215f3364
ge-lx <f4lkebert@gmail.com>
2020-04-22 12:53:16 +0200

    :art: :sparkles: Improve validation

--- backend/utils.js
+++ backend/utils.js
@@ -4,40 +4,48 @@ shortid.characters('αβγδεζηθικλμabcdefghijklmnopqrstuvwxyzABCDEFGHIJ
 module.exports = {
 	createShortId: shortid.generate,
 	validate: (target, schema) => {
-		const schemaProps = Object.keys(schema);
-		const targetProps = Object.keys(target);
-		const output = {};
+		const checkSchema = (target = {}, schema) => {
+			const schemaProps = Object.keys(schema);
+			const targetProps = Object.keys(target);
+			const output = {};
 
-		for (prop of schemaProps) {
-			const targetValue = target[prop];
-			const propSchema = schema[prop];
-			const defaultProvided = propSchema.hasOwnProperty('default');
+			for (prop of schemaProps) {
+				const targetValue = target[prop];
+				const propSchema = schema[prop];
+				const defaultProvided = propSchema.hasOwnProperty('default');
 
-			if (propSchema.type) {
-				const validTypes = Array.isArray(propSchema.type) ? propSchema.type : [propSchema.type];
-				const targetValueType = typeof targetValue;
+				if (propSchema.type) {
+					const validTypes = Array.isArray(propSchema.type) ? propSchema.type : [propSchema.type];
+					const targetValueType = typeof targetValue;
 
-				if (defaultProvided) {
-					validTypes.push('undefined');
+					if (defaultProvided) {
+						validTypes.push('undefined');
+					}
+
+					if (validTypes.includes(targetValueType) === false) {
+						throw new Error(`Validation error! Expected ${prop} to have type ${validTypes.join(' or ')}. Got ${targetValueType}`);
+					}
 				}
 
-				if (validTypes.includes(targetValueType) === false) {
-					throw new Error(`Validation error! Expected ${prop} to have type ${validTypes.join(' or ')}. Got ${targetValueType}`);
+				if (defaultProvided && targetValue === undefined) {
+					output[prop] = propSchema.default;
+				} else {
+					output[prop] = target[prop];
 				}
 			}
 
-			if (defaultProvided && targetValue === undefined) {
-				output[prop] = propSchema.default;
-			} else {
-				output[prop] = target[prop];
+			if (schemaProps.length < targetProps.length) {
+				const extraProps = targetProps.filter(prop => schemaProps.includes(prop) === false);
+				console.warn('Validation warning! You passed unexpected parameters: ', extraProps);
 			}
-		}
 
-		if (schemaProps.length < targetProps.length) {
-			const extraProps = targetProps.filter(prop => schemaProps.includes(prop) === false);
-			console.warn('Validation warning! You passed unexpected parameters: ', extraProps);
-		}
+			return output;
+		};
 
-		return output;
+		// Promise chaining
+		if (schema === undefined) {
+			schema = target;
+			return (target) => checkSchema(target, schema);
+		}
 	}
 };
