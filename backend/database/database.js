0e9f836c6bbae7e82e60ae88ef0e618a5049ce7e
ge-lx <f4lkebert@gmail.com>
2020-04-23 11:32:21 +0200

    :wrench: Fix knex-migrate in production

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,7 +1,9 @@
-const { development } = require('../knexfile');
+const { development } = require('./knexfile');
 const knex_migrate = require('knex-migrate');
 const knex = require('knex');
 
 module.exports = {
-	migrate: async () => await knex_migrate('up', { knexfile: './backend/knexfile.js'}, ({ action, migration }) => console.log(`${action} : ${migration}`)),
+	migrate: async () => await knex_migrate('up',
+		{ knexfile: './backend/database/knexfile.js'},
+		({ action, migration }) => console.log(`${action} : ${migration}`)),
 	knex: knex(development) };
